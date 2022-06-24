b04de600140bbad646668f838308050cf19d48aa
sugarekondrej <ondrej.sugarek@gmail.com>
2022-06-24 11:46:39 +0100

    Implemented Directors model to graphql query

--- api/schema.py
+++ api/schema.py
@@ -6,20 +6,26 @@ from  api import models
 class MovieType(DjangoObjectType):
     class Meta:
         model = models.Movies
-        
+
     movie_age=graphene.String()
 
     def resolve_movie_age(self,info):
         return "old movie" if self.year < 2000 else "New movie"
 
-
+class DirectorType(DjangoObjectType):
+    class Meta:
+        model = models.Director
 
 class Query(graphene.ObjectType):
     all_movies = graphene.List(MovieType)
     movie = graphene.Field(MovieType,id = graphene.Int(),title = graphene.String())
+    all_directors = graphene.List(DirectorType)
 
     def resolve_all_movies(self,info,**kwargs):
         return models.Movies.objects.all()
+
+    def resolve_all_directors(self,info,**kwargs):
+        return models.Director.objects.all()
     
     def resolve_movie(self,info,**kwargs):
         id = kwargs.get("id")
