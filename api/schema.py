c9d0d510156c41bbb2eec0d2acd6183e2c020493
sugarekondrej <ondrej.sugarek@gmail.com>
2022-06-29 18:02:19 +0100

    Inmpelenting Update Mutation

--- api/schema.py
+++ api/schema.py
@@ -50,5 +50,21 @@ class MovieCreateMutation(graphene.Mutation):
         movie = models.Movies.objects.create(title=title, year=year)
 
         return MovieCreateMutation(movie=movie)
+class MovieUpdateMutation(graphene.Mutation):
+    class Arguments:
+        title = graphene.String()
+        year = graphene.Int()
+        id = graphene.ID(required=True)
+    movie = graphene.Field(MovieType)
+
+    def mutate(self,info,id,title,year):
+        movie = models.Movies.objects.get(pk=id)
+        if title is not None:
+            movie.title = title
+        if year is not None:
+            movie.year = year
+        movie.save()
+        return MovieUpdateMutation(movie=movie)
 class Mutation:
     create_movie = MovieCreateMutation.Field()
+    update_movie = MovieUpdateMutation.Field()
